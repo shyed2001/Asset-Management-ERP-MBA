b8c9020aef70ee2697d9c3de31009836f2763282
Shyed Shahriar <48835958+shyed2001@users.noreply.github.com>
2019-11-05 18:47:21 +0600

    Update PyGUI Copy.py

--- PyGUI Copy.py
+++ PyGUI Copy.py
@@ -15,7 +15,12 @@ import tkinter as tk
 from tkinter import filedialog, Text 
 import os
 
-root =tk.TK()
+root = tk.Tk()
+canvas = tk.Canvas(root, height=700, width=700, bg="#263D42")
+canvas.pack()
+
+frame = tk.Frame(root, bg="white")
+frame.place(relwidth=0.8, relheight=0.8)
 
 root.mainloop()
 
