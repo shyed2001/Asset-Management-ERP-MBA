bbaffca11d1210335109351c5e167f54efb269d1
Shyed Shahriar <48835958+shyed2001@users.noreply.github.com>
2019-11-05 18:18:40 +0600

    Create PyGUI Copy.py

---
+++ PyGUI Copy.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        PYGUIautoloader
+# Purpose:     Automate APP programs loading
+#
+# Author:      Shyed Shahriar Housaini
+#
+# Created:     05/11/2019
+# Copyright:   (c) Shyed Shahriar Housaini 2019
+# Licence:     <your licence: Shyed Shahriar Housaini's terms and conditions.>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
