1ab046761624d755c23b6d81f13d91b8c6dc0090
Shyed Shahriar <48835958+shyed2001@users.noreply.github.com>
2019-11-19 13:12:42 +0600

    Update PyGUI Copy.py

--- PyGUI Copy.py
+++ PyGUI Copy.py
@@ -1,28 +1,12 @@
 #-------------------------------------------------------------------------------
-# Name:        PyGUI Copy PYGUIautoloader
-# Purpose:     PYGUIautoloader Automate APP programs loading
+# Name:        Py auto GUI
+# Purpose:
 #
 # Author:      Shyed Shahriar Housaini
 #
-# Created:     05/11/2019
+# Created:     19/11/2019
 # Copyright:   (c) Shyed Shahriar Housaini 2019
 # Licence:     <your licence: Shyed Shahriar Housaini's terms and conditions.>
 #-------------------------------------------------------------------------------
-
-## PYGUIautoloader Automate APP programs loading ##
-
-import tkinter as tk
-from tkinter import filedialog, Text 
-import os
-
-root = tk.Tk()
-canvas = tk.Canvas(root, height=700, width=700, bg="#263D42")
-canvas.pack()
-
-frame = tk.Frame(root, bg="white")
-frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
-openfile = tk.Button(root, text="Open File", padx=10, pady=5)
-root.mainloop()
-
 
 
